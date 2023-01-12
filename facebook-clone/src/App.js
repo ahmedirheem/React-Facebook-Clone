102b964fd46fde5c07dbc294654effab2579b1c4
ahmedirheem <ahmedirheem2003@gmail.com>
2023-01-12 11:34:13 +0200

    Header Ui

---
+++ facebook-clone/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+
+      <Header />
+
+      {/* App body */}
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widget */}
+    </div>
+  );
+}
+
+export default App;
---
+++ facebook-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBhJXFI_dktJfZ1dnZb3oP5eHoactVOSnA",
+    authDomain: "facebook-clone-20f34.firebaseapp.com",
+    projectId: "facebook-clone-20f34",
+    storageBucket: "facebook-clone-20f34.appspot.com",
+    messagingSenderId: "645631269225",
+    appId: "1:645631269225:web:9b03aac58ca600370145f8",
+    measurementId: "G-4FV5WXB2HZ"
+};
